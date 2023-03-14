1d625b8b14053919d62210aa1ce2102d0a4b1542
Brionesgmm <99777937+Brionesgmm@users.noreply.github.com>
2023-03-13 18:27:44 -0600

    answer

---
+++ 8th Kyu/welcome.js
@@ -0,0 +1,28 @@
+// answer
+function greet(language = 'english') {
+    let welcomeLanguages = {
+        english: 'Welcome',
+        czech: 'Vitejte',
+        danish: 'Velkomst',
+        dutch: 'Welkom',
+        estonian: 'Tere tulemast',
+        finnish: 'Tervetuloa',
+        flemish: 'Welgekomen',
+        french: 'Bienvenue',
+        german: 'Willkommen',
+        irish: 'Failte',
+        italian: 'Benvenuto',
+        latvian: 'Gaidits',
+        lithuanian: 'Laukiamas',
+        polish: 'Witamy',
+        spanish: 'Bienvenido',
+        swedish: 'Valkommen',
+        welsh: 'Croeso'
+    }
+
+    if (welcomeLanguages[language] === undefined) {
+        return 'Welcome'
+    }
+
+    return welcomeLanguages[language]
+}
