e55178c755a0e406def00d10377dead2164b2e14
Brionesgmm <99777937+Brionesgmm@users.noreply.github.com>
2023-06-24 19:11:37 -0600

    answer

---
+++ 8th Kyu/logicDrillsTrafficLight.js
@@ -0,0 +1,10 @@
+// answer
+function updateLight(current) {
+  if (current === "green") {
+    return "yellow";
+  } else if (current === "yellow") {
+    return "red";
+  } else {
+    return "green";
+  }
+}
