391e2129b5150a0858ec723c0558c88edf219e3e
Brionesgmm <99777937+Brionesgmm@users.noreply.github.com>
2023-08-21 13:32:56 -0600

    answer

---
+++ 8th Kyu/drinkAbout.js
@@ -0,0 +1,12 @@
+// answer
+function peopleWithAgeDrink(old) {
+  if (old <= 13) {
+    return "drink toddy";
+  } else if (old <= 17) {
+    return "drink coke";
+  } else if (old <= 21) {
+    return "drink beer";
+  } else {
+    return "drink whisky";
+  }
+}
