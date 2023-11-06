e15ad1ded94994cdd35d2ab75d8894a63ac6419f
Juan Briones <jbrionesgmm@gmail.com>
2023-11-06 11:29:00 -0700

    answer

---
+++ LeetCode/counter2.js
@@ -0,0 +1,16 @@
+// answer
+var createCounter = function (init) {
+  let count = init;
+  return {
+    increment() {
+      return ++count;
+    },
+    decrement() {
+      return --count;
+    },
+    reset() {
+      count = init;
+      return count;
+    },
+  };
+};
