40f3d07019b56d2175a88ae5c6d742c313bfa8e7
unknown <jbrionesgmm@gmail.com>
2024-02-02 10:33:36 -0700

    answer

---
+++ 8th Kyu/basicMathOperations.js
@@ -0,0 +1,12 @@
+// answer
+function basicOp(operation, value1, value2) {
+  if (operation === "+") {
+    return value1 + value2;
+  } else if (operation === "-") {
+    return value1 - value2;
+  } else if (operation === "*") {
+    return value1 * value2;
+  } else if (operation === "/") {
+    return value1 / value2;
+  }
+}
