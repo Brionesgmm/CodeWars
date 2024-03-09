f2edc102025eb07af6d962b199e63cdae6bae411
Juan Briones <jbrionesgmm@gmail.com>
2024-03-09 09:13:56 -0700

    answer

---
+++ 8th Kyu/drawStairs.js
@@ -0,0 +1,11 @@
+// answer
+function drawStairs(n) {
+  let stairs = "";
+  for (let i = 0; i < n; i++) {
+    stairs += " ".repeat(i) + "I";
+    if (i < n - 1) {
+      stairs += "\n";
+    }
+  }
+  return stairs;
+}
