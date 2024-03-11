77b3f71c62942875563cd63467ed811997818416
Juan Briones <jbrionesgmm@gmail.com>
2024-03-11 10:42:34 -0600

    answer

---
+++ 8th Kyu/defineACardSuit.js
@@ -0,0 +1,12 @@
+// answer
+function defineSuit(card) {
+  if (card.includes("♣")) {
+    return "clubs";
+  } else if (card.includes("♦")) {
+    return "diamonds";
+  } else if (card.includes("♥")) {
+    return "hearts";
+  } else {
+    return "spades";
+  }
+}
